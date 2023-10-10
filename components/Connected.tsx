63858447275de4ab0edf5e8b3db29b2c9e4f9531
Joel <varghesejoel6@gmail.com>
2023-10-11 01:19:28 +0530

    final update

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -11,13 +11,7 @@ import { ArrowForwardIcon } from "@chakra-ui/icons"
 import { useConnection, useWallet } from "@solana/wallet-adapter-react"
 import toast from 'react-hot-toast';
 import CandyMachineNFTs from "./CandyMachineNFTs"
-import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import { generateSigner, transactionBuilder, publicKey, some } from '@metaplex-foundation/umi';
-import { fetchCandyMachine, mintV2, mplCandyMachine, safeFetchCandyGuard } from "@metaplex-foundation/mpl-candy-machine";
-import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
-import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata';
-import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox';
-import { clusterApiUrl } from '@solana/web3.js';
+
 
 const Connected: FC = () => {
 
